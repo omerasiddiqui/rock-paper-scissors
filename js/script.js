4699319adecd1a6d5b998c5da1f1ffb5d64466cc
Omer Siddiqui <oasiddiqui1@gmail.com>
2018-05-28 14:52:58 -0400

    widened the wrapper and refactored JS

--- js/script.js
+++ js/script.js
@@ -1,119 +1,133 @@
 // let userChoice = prompt('rock, papers, or scissors?');
 
-  let playerOne = 0;
-  let playerTwo = 0;
+let playerOne = 0;
+let playerTwo = 0;
 
-  let scoreCard = document.querySelector('#scoreCard');
-  let resultUser = document.querySelector("#resultUser");
-  let resultComputer = document.querySelector("#resultComputer");
+init();
 
+function init() {
+  rock.addEventListener("click", function() {
+    const rock = document.querySelector("#rock");
+    userChoice = "rock";
+    runGame();
+  })
 
-  let rock = document.querySelector("#rock");
-  let paper = document.querySelector("#paper");
-  let scissors = document.querySelector("#scissors");
+  paper.addEventListener("click", function() {
+    const paper = document.querySelector("#paper");
+    userChoice = "paper";
+    runGame();
+  })
 
-  let restart = document.querySelector(".restart");
+  scissors.addEventListener("click", function() {
+    const scissors = document.querySelector("#scissors");
+    userChoice = "scissors";
+    runGame();
+  });
+}
 
-  init();
+function runGame() {
+  let winner = 5;
 
-  function init() {
-    rock.addEventListener("click", function() {
-      userChoice = "rock";
-      runGame();
-    })
+  let computerChoice = Math.random() * 10;
 
-    paper.addEventListener("click", function() {
-      userChoice = "paper";
-      runGame();
-    })
-
-    scissors.addEventListener("click", function() {
-      userChoice = "scissors";
-      runGame();
-    });
+  if (computerChoice <= 3.33) {
+    computerChoice = "rock";
+  } else if (computerChoice <= 6.66) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
   }
 
-  function runGame() {
-    let winner = 5;
-
-    let computerChoice = Math.random() * 10;
-
-
-    if (computerChoice <= 3.33) {
-      computerChoice = "rock";
-    } else if (computerChoice <= 6.66) {
-      computerChoice = "paper";
-    } else {
-      computerChoice = "scissors";
-    }
-
-
-
-    function compare(choice1, choice2) {
-      if (choice1 === choice2) {
-        return "The result is a tie!";
-      } else if (choice1 === "rock") {
-        if (choice2 === "scissors") {
-          playerOne++;
-          return "rock wins!";
-        } else {
-          playerTwo++;
-          return "paper wins!";
-        }
-      } else if (choice1 === "paper") {
-        if (choice2 === "rock") {
-          playerOne++;
-          return "paper wins!";
-        } else {
-          playerTwo++;
-          return "scissors wins!";
-        }
-      } else if (choice1 === "scissors") {
-        if (choice2 === "paper") {
-          playerOne++;
-          return "scissors wins!";
-        } else {
-          playerTwo++;
-          return "rock wins!";
-        }
+  function compare(choice1, choice2) {
+    if (choice1 === choice2) {
+      return "The result is a tie!";
+    } else if (choice1 === "rock") {
+      if (choice2 === "scissors") {
+        playerOne++;
+        return "rock wins!";
+      } else {
+        playerTwo++;
+        return "paper wins!";
+      }
+    } else if (choice1 === "paper") {
+      if (choice2 === "rock") {
+        playerOne++;
+        return "paper wins!";
+      } else {
+        playerTwo++;
+        return "scissors wins!";
+      }
+    } else if (choice1 === "scissors") {
+      if (choice2 === "paper") {
+        playerOne++;
+        return "scissors wins!";
+      } else {
+        playerTwo++;
+        return "rock wins!";
       }
     }
+  }
 
-    function winnerResult(playerOne, playerTwo) {
-
+  function winnerResult(playerOne, playerTwo) {
+    const scoreCard = document.querySelector('#scoreCard');
+    const resultUser = document.querySelector("#resultUser");
+    const resultComputer = document.querySelector("#resultComputer");
     if (playerOne == winner) {
       scoreCard.classList.add("hide");
       resultUser.classList.remove("hide");
       console.log("User WINS!");
-  } else if (playerTwo == winner) {
+    } else if (playerTwo == winner) {
       scoreCard.classList.add("hide");
       resultComputer.classList.remove("hide");
       console.log("Computer WINS!");
     }
   };
 
-    var totals = compare(userChoice, computerChoice);
-
-    winnerResult(playerOne, playerTwo);
+  function logInfo() {
+    const scoreUser = document.querySelector('.scoreUser')
+    const scoreComputer = document.querySelector('.scoreComputer');
+    const user = document.querySelector('.user')
+    const computer = document.querySelector('.computer')
 
     console.log("User choice is: " + userChoice);
-    document.querySelector('.user').innerHTML = "User's choice is: " + userChoice;
+    user.innerHTML = "User's choice is: " + userChoice;
     console.log("Computer choice is: " + computerChoice);
-    document.querySelector('.computer').innerHTML = "Computer's choice is: " + computerChoice;
+    computer.innerHTML = "Computer's choice is: " + computerChoice;
 
     console.log(totals);
-    document.querySelector('.winner').innerHTML = "The result of the match is: " + totals;
+    winner.innerHTML = "The result of the match is: " + totals;
 
-    document.querySelector('.scoreUser').innerHTML = "User score is: " + playerOne;
-    document.querySelector('.scoreComputer').innerHTML = "Computer score is: " + playerTwo;
+    scoreUser.innerHTML = "User score is: " + playerOne;
+    scoreComputer.innerHTML = "Computer score is: " + playerTwo;
 
+    if(playerOne === winner || playerTwo === winner){
+      playerOne = 0;
+      playerTwo =0
+      scoreUser.innerHTML = "User score is: " + playerOne;
+      scoreComputer.innerHTML = "Computer score is: " + playerTwo;
+    }
+  }
+
+  var totals = compare(userChoice, computerChoice);
+
+  winnerResult(playerOne, playerTwo);
+
+  logInfo();
 
 };
 
-restart.addEventListener("click", function(){
+restartU.addEventListener("click", function() {
+  const restartU = document.querySelector("#restartU");
   resultComputer.classList.add("hide");
   resultUser.classList.add("hide");
   scoreCard.classList.remove("hide");
-  playerOne = 0;
-  playerTwo = 0;
+  console.log('game restarted!');
+});
+
+restartC.addEventListener("click", function() {
+  const restartC = document.querySelector("#restartC");
+  resultComputer.classList.add("hide");
+  resultUser.classList.add("hide");
+  scoreCard.classList.remove("hide");
+  console.log('game restarted!');
 });
